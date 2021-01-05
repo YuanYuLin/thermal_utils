35c82f15cbf3817b6092adf12b05185527a84de8
yyl <yuanyu.lin@gmail.com>
2021-01-05 17:59:36 +0800

    update.

--- js/widget-add_condition.js
+++ js/widget-add_condition.js
@@ -1,1 +1,131 @@
-custom-widget/widget-add_condition.js
+$.widget("custom.add_condition",{
+  options: {
+	  dao:[],
+	  condition_count:0,
+	  test:'test'
+  },
+  _create: function() {
+    this.add_more_condition=$("<a>", {
+      text: "+"
+    }).appendTo(this.element).button();
+	
+	var container = $("<div/>");
+	container.attr("id", "conditions_container");
+	container.appendTo(this.element);
+
+    this._on(this.add_more_condition, {
+      click: "add_a_condition"
+    });
+  },
+  outputjson:function() {
+    var cond_list = [];
+    var groups = $("#condition_groups\\[\\]");
+    var items = $("#condition_items\\[\\]");
+    var ops_list = $("#condition_ops\\[\\]");
+    var inst_list =$("#condition_inst\\[\\]");
+    var text_list =$("#condition_text\\[\\]");
+    for(i=0;i<groups.length;i++){
+      var group = groups[i];
+      var item = items[i];
+      var ops = ops_list[i];
+      var inst = inst_list[i];
+      var text = text_list[i];
+      var cond = {};
+      cond.keyprefix=group.value + "." + item.value;
+      cond.instance = inst.value;
+      cond.ops = ops.value;
+      cond.value = text.value;
+      cond_list.push(cond);
+    }
+    return cond_list;
+  },
+  reset_widget: function() {
+    this.options.condition_count = 0;
+	$("#conditions_container").empty();
+  },
+  change_group: function(event){
+	var parentid = event.target.parentElement.id;
+	var condition_select = $("#" + parentid + " > #condition_groups\\[\\]");
+	var condition_select2 = $("#" + parentid + " > #condition_items\\[\\]");
+        var condition_select3 = $("#" + parentid + " > #condition_ops\\[\\]");
+        var condition_select4 = $("#" + parentid + " > #condition_inst\\[\\]");
+	condition_select2.empty()
+	condition_select3.empty()
+	condition_select4.empty()
+	var selected_value = condition_select.val();
+    var _dao_list = this.options.dao;
+    for(i=0;i<_dao_list.length;i++) {
+      var _dao = _dao_list[i];
+      if(selected_value == _dao.TDEV) {
+        for(j=0;j<_dao.DAO_LIST.length;j++) {
+          sub_item = _dao.DAO_LIST[j];
+          var sub_item_option = $("<option />");
+          sub_item_option.html(sub_item);
+          sub_item_option.val(sub_item);
+          sub_item_option.appendTo(condition_select2);
+        }
+        for(j=0;j<_dao.OPS_LIST.length;j++) {
+          _item = _dao.OPS_LIST[j];
+          var _option = $("<option />");
+          _option.html(_item.DISPLAY);
+          _option.val(_item.NAME);
+          _option.appendTo(condition_select3);
+	}
+        for(j=0;j<_dao.INST_LIST.length;j++) {
+          _item = _dao.INST_LIST[j];
+          var _option = $("<option />");
+          _option.html(_item.DISPLAY);
+          _option.val(_item.NAME);
+          _option.appendTo(condition_select4);
+	}
+      }
+    }
+  },
+ _setOption: function(key, value) {
+	if(key === "dao") {
+		this.options.dao = value;
+	}
+ },
+ _setOptions: function(options) {
+	 var that = this;
+	 $.each(options, function(key, value){
+		 that._setOption(key, value);
+	 });
+ },
+ add_a_condition: function(event){
+    var container = $("#conditions_container");
+    var sep = $("<div/>");
+	sep.attr("id", "condition_container" + this.options.condition_count);
+	this.options.condition_count+=1;
+    var condition_select = $("<select  />");
+    condition_select.attr("id", "condition_groups[]");
+	this._on(condition_select, {
+		change: "change_group"
+	});
+	var condition_select2 = $("<select  />");
+	condition_select2.attr("id", "condition_items[]");
+
+var condition_select3 = $("<select  />");
+condition_select3.attr("id", "condition_ops[]");
+var condition_select4 = $("<select  />");
+condition_select4.attr("id", "condition_inst[]");
+var condition_text = $("<input/>");
+condition_text.attr("id", "condition_text[]");
+condition_text.width(30);
+    sep.appendTo(container);
+   condition_select.appendTo(sep);
+   condition_select4.appendTo(sep);	
+   condition_select2.appendTo(sep);
+   condition_select3.appendTo(sep);
+   condition_text.appendTo(sep);	
+
+var _dao_list = this.options.dao;
+    for(i=0;i<_dao_list.length;i++){
+	  var _dao = _dao_list[i];
+      var condition_option = $("<option />");
+	  condition_option.html(_dao.TDEV);
+      condition_option.val(_dao.TDEV);
+	  condition_option.appendTo(condition_select);
+    }
+  }
+});
--- js/widget-add_result.js
+++ js/widget-add_result.js
@@ -1,1 +1,59 @@
-custom-widget/widget-add_result.js
+$.widget("custom.add_result",{
+  options: {
+	  dao:[],
+	  condition_count:0,
+	  test:'test'
+  },
+  outputjson: function() {
+	obj = {};
+    var group_r = $("#r_group");
+    var item_r = $("#r_item");
+    var inst_r = $("#r_inst");
+	obj.keyprefix = group_r.val() + "." + item_r.val();
+	obj.inst = inst_r.val();
+	return obj;
+  },
+  reset_widget: function() {
+    var item_r = $("#r_item");
+    item_r.val('');
+  },
+  _create: function() {
+    var sep_r = $("<div/>");
+    var label_r = $("<label/>");
+    var input_group_r = $("<input/>");
+    var label_dot_r = $("<label/>");
+    var input_item_r = $("<input/>");
+    var label_inst_r = $("<label/>");
+    var select_inst_r = $("<select/>");
+    var inst_option_r = $("<option/>");
+    input_group_r.attr("id", "r_group");
+    input_group_r.val("common_thermal_algorithm_result")
+    input_group_r.attr("disabled", "disabled");
+    label_dot_r.html(".");
+    input_item_r.attr("id", "r_item");
+    select_inst_r.attr("id", "r_inst");
+    inst_option_r.html("1");
+    inst_option_r.val("1");
+    label_r.html("R:");
+    label_inst_r.html("Inst:");
+    label_r.appendTo(sep_r);
+    input_group_r.appendTo(sep_r);
+    label_dot_r.appendTo(sep_r);
+    input_item_r.appendTo(sep_r);
+    label_inst_r.appendTo(sep_r);
+    inst_option_r.appendTo(select_inst_r);
+    select_inst_r.appendTo(sep_r);
+    sep_r.appendTo(this.element);
+  },
+ _setOption: function(key, value) {
+	if(key === "dao") {
+		this.options.dao = value;
+	}
+ },
+ _setOptions: function(options) {
+	 var that = this;
+	 $.each(options, function(key, value){
+		 that._setOption(key, value);
+	 });
+ }
+});
